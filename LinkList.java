b796b5ea9ccd3257973b53a5dfe5ff4d07d02a1e
Hemant Tripathi <hemtri1984@gmail.com>
2020-02-08 12:33:07 +1030

    Linear Linklist program in Java

---
+++ LinkList.java
@@ -0,0 +1,24 @@
+/**
+ * Author: Hemant Tripathi
+ */
+
+
+public class LinkList {
+	
+	public static void main(String[] args) {
+		while(true) {
+			System.out.println("Selection an option...");
+			System.out.println("1. Add a node at the beginning of list");
+			System.out.println("2. Add a node in the middle of list");
+			System.out.println("3. Add a node at the end of list");
+			System.out.println("4. Remove a node from the start of list");
+			System.out.println("5. Remove a node from the middle of list");
+			System.out.println("6. Remove a node from the end of list");
+			System.out.println("7. Show all nodes of the list");
+			System.out.println("8. Exit");
+			
+			
+		}
+	}
+
+}
